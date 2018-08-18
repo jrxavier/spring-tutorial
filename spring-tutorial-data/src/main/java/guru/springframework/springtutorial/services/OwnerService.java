19038ff73d1cb27c9d2eda1de2b911797d09f8bf
Jose Xavier <jrxavier@gmail.com>
2018-08-18 11:03:45 -0300

    Refactoring to common Crud interface. Closes #11.

--- spring-tutorial-data/src/main/java/guru/springframework/springtutorial/services/OwnerService.java
+++ spring-tutorial-data/src/main/java/guru/springframework/springtutorial/services/OwnerService.java
@@ -4,10 +4,8 @@ import guru.springframework.springtutorial.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLasName(String lasName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
--- spring-tutorial-data/src/main/java/guru/springframework/springtutorial/services/PetService.java
+++ spring-tutorial-data/src/main/java/guru/springframework/springtutorial/services/PetService.java
@@ -4,10 +4,7 @@ import guru.springframework.springtutorial.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
 
 }
