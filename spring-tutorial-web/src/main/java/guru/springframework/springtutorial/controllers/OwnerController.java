20fc32ad56d78b30727be6cc3e90f5fb15b81433
Jose Xavier <jrxavier@gmail.com>
2018-08-20 09:39:26 -0300

    Lista de Owners. Closes #16

--- spring-tutorial-web/src/main/java/guru/springframework/springtutorial/controllers/OwnerController.java
+++ spring-tutorial-web/src/main/java/guru/springframework/springtutorial/controllers/OwnerController.java
@@ -1,14 +1,29 @@
 package guru.springframework.springtutorial.controllers;
 
+import guru.springframework.springtutorial.model.Owner;
+import guru.springframework.springtutorial.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+
+        Set<Owner> ownerSet = ownerService.findAll();
+
+        model.addAttribute("owners", ownerSet);
 
         return "owners/index";
     }
