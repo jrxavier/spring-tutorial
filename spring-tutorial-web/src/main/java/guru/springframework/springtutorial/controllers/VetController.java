2280421960931d492816cba1a759cd5da5045f22
Jose Xavier <jrxavier@gmail.com>
2018-08-21 21:39:17 -0300

    Lista de Vets. Closes #15

--- spring-tutorial-web/src/main/java/guru/springframework/springtutorial/controllers/VetController.java
+++ spring-tutorial-web/src/main/java/guru/springframework/springtutorial/controllers/VetController.java
@@ -1,13 +1,28 @@
 package guru.springframework.springtutorial.controllers;
 
+import guru.springframework.springtutorial.model.Vet;
+import guru.springframework.springtutorial.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        Set<Vet> vetSet = vetService.findAll();
+
+        model.addAttribute("vets", vetSet);
 
         return "vets/index";
     }
