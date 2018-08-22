3787063866bff252ea26da619ef6499f0b43cf60
Jose Xavier <jrxavier@gmail.com>
2018-08-21 22:05:07 -0300

    Refactor para inclusão de método de geração de ID. Interessante a abordagem de extender os tipos genéricos. Closes #23

--- spring-tutorial-web/src/main/java/guru/springframework/springtutorial/bootstrap/DataLoader.java
+++ spring-tutorial-web/src/main/java/guru/springframework/springtutorial/bootstrap/DataLoader.java
@@ -24,14 +24,14 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+        //owner1.setId(1L);
         owner1.setFirstName("Jose");
         owner1.setLastName("Xavier");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+        //owner2.setId(2L);
         owner2.setFirstName("Vanessa");
         owner2.setLastName("Xavier");
 
@@ -40,14 +40,14 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("-----------Loaded owners-----------------");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        //vet1.setId(1L);
         vet1.setFirstName("Maria");
         vet1.setLastName("Cardoso");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+        //vet2.setId(2L);
         vet2.setFirstName("Monica");
         vet2.setLastName("Cardoso");
 
